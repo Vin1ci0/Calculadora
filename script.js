248d7193e8d1a929fdb74df42eeb39b8fff6f989
Vinicio Oliveira <oliveiravinicios31@gmail.com>
2022-01-24 10:57:45 -0300

    Tudo concluído, calculadora pronta!

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function Calcular () {
-    
+function inserir (num) {
+    var numero = document.getElementById('Resultado').innerHTML 
+    document.getElementById('Resultado').innerHTML = numero + num
+}
+function clean () {
+    document.getElementById('Resultado').innerHTML = ''
+}
+function back() {
+    var resultado = document.getElementById('Resultado').innerHTML
+    document.getElementById('Resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular () {
+    var resultado = document.getElementById('Resultado').innerHTML
+    if (resultado) {
+        document.getElementById('Resultado').innerHTML = eval(resultado)
+    }
+    else {
+        document.getElementById('Resultado').innerHTML = "Nada para calcular"
+    }
 }
